b37225d3ad9621506cd045b5afb524a29e92a130
Jjunjiii’ <drum1701@gmail.com>
2024-12-07 22:28:17 +0900

    feat : setting

---
+++ src/main/java/richmaker/richmaker/RichMakerApplication.java
@@ -0,0 +1,13 @@
+package richmaker.richmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RichMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/richmaker/richmaker/RichmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package richmaker.richmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RichmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
